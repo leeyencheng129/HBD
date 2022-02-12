273856ca06d1ae52de22f793a20e8bb9688ab48a
Philip Bagus Kristianto <philscoidn3@gmail.com>
2022-02-12 18:34:50 +0700

    cek node
    Signed-off-by: Philip Bagus Kristianto <philscoidn3@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,11 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import ElementUI from 'element-ui';
 import router from './route/route.js';
-import VuePageTransition from 'vue-page-transition'
-import VueYoutube from 'vue-youtube'
-import VueAudio from 'vue-audio';
-import './styles/UI-theme/index.css'
+import VuePageTransition from 'vue-page-transition';
+import VueYoutube from 'vue-youtube';
+import './styles/UI-theme/index.css';
 
 
 Vue.config.productionTip = false;
@@ -13,7 +12,6 @@ Vue.config.productionTip = false;
 Vue.use(ElementUI);
 Vue.use(VuePageTransition);
 Vue.use(VueYoutube);
-Vue.use(VueAudio);
 
 new Vue({
     render: h => h(App),
