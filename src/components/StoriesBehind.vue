7f0faed8541648922e39479bb87bb15277820419
Philip Bagus Kristianto <32977603+Philipsco@users.noreply.github.com>
2022-02-24 23:40:41 +0700

    delete love

--- src/components/StoriesBehind.vue
+++ src/components/StoriesBehind.vue
@@ -43,7 +43,7 @@
                             Deploy your birthday gift to Website so you can see
                         </p>
                         <p>
-                            I hope you like it 🤍
+                            I hope you like it
                         </p>
                         <p style="font-size: 0.25rem">
                             Deep down from my heart, I Miss You. a lot. and somehow Naneun Jhoahe - PBK
